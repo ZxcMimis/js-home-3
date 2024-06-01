cdb9a829b2fd6cc20cbe121fa12f47cd1251871c
Руслан Івасюк <126970988+ZxcMimis@users.noreply.github.com>
2024-06-01 16:47:01 +0300

    js script

---
+++ index.js
@@ -0,0 +1,19 @@
+const result = 5 + 5 + `5`;
+console.log(typeof result);
+console.log(result);
+console.log(" ");
+
+const email = "qruslan.2010iw@gmail.com"
+console.log(email.includes("@"))
+console.log(email.length)
+console.log(" ");
+
+const my = "my";
+const name = "name";
+const is = "is";
+const fullname = `${my} ${name} ${is} Ruslan`;
+console.log(fullname);
+
+const username = "zitraks";
+const payment = "1400";
+alert(`Дякуємо, ${username}! До сплати ${payment} гривень.`);
